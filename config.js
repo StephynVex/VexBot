a4dba4db77272f602a93f2052d916350cd6e4896
StephynVex <100586342+StephynVex@users.noreply.github.com>
2022-04-05 17:12:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs' , 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad 
 global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
-global.owner = ['595985902159'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
-global.mods = ['595985902159'] // No tiene utilidad
-global.prems = ['595985902159'] // Podra usar algunos comandos del apartado de propietario
+global.owner = ['595985902159,595985851690'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
+global.mods = ['595985902159,595985851690'] // No tiene utilidad
+global.prems = ['595985902159,595985851690'] // Podra usar algunos comandos del apartado de propietario
 
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = 'The Shadow Brokers - Bot'
+global.author = 'The VexBot - Bot'
 
 
-global.wm = '©The Shadow Brokers - Bot'
+global.wm = '©The VexBot - Bot'
 global.wait = '*Espere un momento..*'
 
 
